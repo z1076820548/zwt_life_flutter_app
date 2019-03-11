12347e03f9dff046aa802f91adcfb0dff292801e
z1076820548 <43718433+z1076820548@users.noreply.github.com>
2019-03-11 17:54:06 +0800

    2019.3.11

---
+++ lib/common/widgets/homewidget/HomeTopBar.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class HomeTopBar extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+    return null;
+  }
+
+}
